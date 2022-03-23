f3ee483a7db38859a64aaa3463e3de257c5af47d
Rafael Alexander <noesrafa@gmail.com>
2022-03-22 21:17:35 -0600

    :rocket:

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default function Login () {
+    return (
+        <LoginStyled>
+            Hola
+        </LoginStyled>
+    )
+}
+
+const LoginStyled = styled.div`
+    background-color: green;
+`
